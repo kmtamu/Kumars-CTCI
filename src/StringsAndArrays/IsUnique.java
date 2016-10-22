36edd35717e21c5c9d6c96de3f2af190c139f74a
kmtamu <kmr.manish@tamu.edu>
2016-10-21 22:38:14 -0500

    CTCI problems
    Problems in Cracking the Coding Interview book

---
+++ src/StringsAndArrays/IsUnique.java
@@ -0,0 +1,11 @@
+package StringsAndArrays;
+
+/**
+ * Created by Kumar on 10/21/2016.
+ */
+public class IsUnique {
+    public static void main(String[] arg){
+        String Input="AbcdAefgd";
+        System.out.println(Input);
+    }
+}
