a2641a6a217b5bfff9e1c7077c0d6a039f3b1fd6
kmtamu <kmr.manish@tamu.edu>
2016-10-22 01:11:26 -0500

    Part 1 done

--- src/StringsAndArrays/IsUnique.java
+++ src/StringsAndArrays/IsUnique.java
@@ -5,7 +5,25 @@ package StringsAndArrays;
  */
 public class IsUnique {
     public static void main(String[] arg){
-        String Input="AbcdAefgd";
-        System.out.println(Input);
+        String Input="AbcdefAa";
+        char Inchar[] = Input.toCharArray();
+        boolean Result = IsUnique(Inchar);
+        System.out.println("String has all unique charcters? "+Result);
+    }
+
+
+    // Using unicode value of char as index of another boolean array
+    static boolean IsUnique(char[] Inchar){
+        boolean[] MapArray = new boolean[256];
+        for (int i=0;i<Inchar.length;i++){
+            int index = (int) Inchar[i];
+            if(MapArray[index]==true){
+                return false;
+            }
+            else{
+                MapArray[index]=true;
+            }
+        }
+        return true;
     }
 }
