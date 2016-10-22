2aea5d817b20be8a78040ad496d7b47aeb3cc8e7
kmtamu <kmr.manish@tamu.edu>
2016-10-22 12:36:13 -0500

    Method 2 implemented

--- src/StringsAndArrays/CheckPermutation.java
+++ src/StringsAndArrays/CheckPermutation.java
@@ -1,16 +1,20 @@
 package StringsAndArrays;
 
+import java.util.Arrays;
+
 /**
  * Created by Kumar on 10/22/2016.
  */
 public class CheckPermutation {
     public static void main(String arg[]){
         String Input1 ="top dad";
-        String Input2 ="add opt";
+        String Input2 ="ad opt";
         char[] InputChar1 = Input1.toCharArray();
         char[] InputChar2 = Input2.toCharArray();
         boolean result = Method1(InputChar1,InputChar2);
         System.out.println("Strings are permutation? "+ result);
+        boolean result2 = Method2(InputChar1,InputChar2);
+        System.out.println("Strings are permutation? "+ result2);
     }
 
     //Method 1 using integer arrays to compare character counts. Spaces count too!
@@ -32,5 +36,20 @@ public class CheckPermutation {
             return true;
         }
         return false;
+    }
+
+
+    //Method 2: Sort each array and compare element wise!
+    static boolean Method2(char[] In1, char[]In2){
+        Arrays.sort(In1);
+        Arrays.sort(In2);
+        if(In1.length==In2.length){
+            for (int i=0;i<In1.length;i++){
+                if (In1[i]!=In2[i]) return false;
+
+            }
+        }
+        else return false;
+        return true;
     }
 }
